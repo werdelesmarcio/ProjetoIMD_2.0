faa15446d5ea802e195226fd8d8e8745ae9331cc
Werdeles Marcio de C. Soares <36682515+werdelesmarcio@users.noreply.github.com>
2020-11-29 22:14:39 -0300

    Create animais.hpp

---
+++ lib/animais.hpp
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <vector>
+#include <string>
+
+using std::string;
+using std::vector;
+
+class Animais
+{
+public:
+    Animais(string tipo, string raca, string idade, string codigo);
+    ~Animais();
+    string getTipo() const;
+    string getRaca() const;
+    string getIdade() const;
+    string getCodigo() const;
+
+    /** Este método lê os dados do animal.
+         * puxando os métodos que irão
+         * preencher e efetivar a operação
+        **/
+
+    //Interface com os usuários do sistema
+    void novoAnimal();
+    void removeAnimal();
+    void vendeAnimal();
+    void listaAnimal();
+
+    //Metodos para realizar as operações
+
+    void imprimeListaAnimais(string codigo);
+
+private:
+    // Método interno usado para verificar o cadastro do animal
+    string tipo;
+    string raca;
+    string idade;
+    string codigo;
+};
