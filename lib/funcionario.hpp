ea2bc0d16361dd11bfcce1171e478e3d615e3ee5
ThaysFelix-G <71108281+ThaysFelix-G@users.noreply.github.com>
2020-11-29 23:18:30 -0300

    Abstracoes iniciais da classe Funcionario

---
+++ lib/funcionario.hpp
@@ -0,0 +1,57 @@
+#ifndef _FUNCIONARIO_
+#define _FUNCIONARIO_
+
+#include <iostream>
+#include <list>
+
+#include "veterinario.hpp"
+#include "tratador.hpp"
+#include "profissional.hpp"
+
+using namespace std;
+
+enum especialidade
+{
+    clinicoGeral,
+    cirurgiao,
+    anestesista,
+    cardiologista,
+    neurologista,
+    psicologo,
+    nutricionista
+
+};
+
+enum funcao
+{
+    recepcionista,
+    limpeza,
+    motorista,
+    limpezaLocalAnimais,
+    preparacaoDeAlimentos
+
+};
+
+class Funcionario {
+
+private:
+    //string nome;   
+    Veterinario* veterinario[];
+    //Tratador* tratador[];
+    //Profissional* profissional[];
+    int qtdProfissionais = 0;
+    int qtdVeterinarios = 0;
+    int qtdTratadores = 0;
+
+public:
+    //string getNome();
+    //void setNome(string nome_);
+    void addVeterinario(Veterinario* novo);
+    void removVeterinario(string nome);
+    void addTratador(Tratador* novo);
+    void removTratador(string nome);
+    void addProfissional(Tratador* novo);
+    void removProfissional(string nome);
+};
+
+#endif
