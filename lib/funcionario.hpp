548c862ddec4778cd383b4922128467fe819b8ee
ThaysFelix-G <71108281+ThaysFelix-G@users.noreply.github.com>
2020-11-30 22:48:30 -0300

    Adicionado construtores e destrutores e herança

--- lib/funcionario.hpp
+++ lib/funcionario.hpp
@@ -4,23 +4,7 @@
 #include <iostream>
 #include <list>
 
-#include "veterinario.hpp"
-#include "tratador.hpp"
-#include "profissional.hpp"
-
 using namespace std;
-
-enum especialidade
-{
-    clinicoGeral,
-    cirurgiao,
-    anestesista,
-    cardiologista,
-    neurologista,
-    psicologo,
-    nutricionista
-
-};
 
 enum funcao
 {
@@ -34,24 +18,29 @@ enum funcao
 
 class Funcionario {
 
-private:
-    //string nome;   
-    Veterinario* veterinario[];
-    //Tratador* tratador[];
-    //Profissional* profissional[];
-    int qtdProfissionais = 0;
-    int qtdVeterinarios = 0;
-    int qtdTratadores = 0;
+//pode ser acessado pela propria classee pelos metodos derivados da classe(subclasses)
+protected:
+    string nome; 
+    string telefone;
+    string email;
+    string endereco;  
 
 public:
-    //string getNome();
-    //void setNome(string nome_);
-    void addVeterinario(Veterinario* novo);
-    void removVeterinario(string nome);
-    void addTratador(Tratador* novo);
-    void removTratador(string nome);
-    void addProfissional(Tratador* novo);
-    void removProfissional(string nome);
+   
+    Funcionario();
+    Funcionario(string nome, string telefone, 
+                  string email, string endereco);
+    ~Funcionario();
+    string getNome();
+    string getTelefone();
+    string getEmail();
+    string getEndereco();
+
+    void setNome(string nome);
+    void setTelefone(string telefone);
+    void setEmail(string email);
+    void setEndereco(string endereco);
+
 };
 
 #endif
