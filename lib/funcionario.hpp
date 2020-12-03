eb5aa334d5c13e9a0062ba9b3dd774a7a58d0455
ThaysFelix-G <71108281+ThaysFelix-G@users.noreply.github.com>
2020-12-03 17:50:06 -0300

    Add files via upload

--- lib/funcionario.hpp
+++ lib/funcionario.hpp
@@ -6,6 +6,10 @@
 #include <iostream>
 #include <list>
 
+#include "veterinario.hpp"
+
+#define CAPACIDADE_MAX 5
+
 using namespace std;
 
 enum funcao
@@ -18,30 +22,42 @@ enum funcao
 
 };
 
-class Funcionario
-{
+class Funcionario {
 
-    //pode ser acessado pela propria classee pelos metodos derivados da classe(subclasses)
+//atributos pode ser acessado pela propria classe pelos metodos derivados da classe(subclasses)
 protected:
-    string nome;
+    string id;
+    string nome; 
     string telefone;
     string email;
     string endereco;
+    int qtd_funcionario = 0;
+
+private:
+    Veterinario* veterinario[CAPACIDADE_MAX];
 
 public:
+   
     Funcionario();
-    Funcionario(string nome, string telefone,
-                string email, string endereco);
+    Funcionario(string id, string nome, string telefone, 
+                  string email, string endereco);
     ~Funcionario();
+    string getId();
     string getNome();
     string getTelefone();
     string getEmail();
     string getEndereco();
 
+    void setId(string id);
     void setNome(string nome);
     void setTelefone(string telefone);
     void setEmail(string email);
     void setEndereco(string endereco);
+
+    void addVeterinario(Veterinario* novo);
+	void remVeterinario(string nome);
+	void listarVeterinarios();
+
 };
 
 #endif
